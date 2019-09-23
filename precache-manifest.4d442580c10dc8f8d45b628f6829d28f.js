b87fc5267529dada2e168f954e0e514d24049694
Keliang Liu <keliangliu1@gmail.com>
2019-09-24 01:20:07 +1000

    Updates

--- precache-manifest.4d442580c10dc8f8d45b628f6829d28f.js
+++ precache-manifest.4d442580c10dc8f8d45b628f6829d28f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5fc4824b820049d3d9bebc48d7d59ba4",
+    "revision": "482098e524966d629bfd2f0b6ad2ae92",
     "url": "/react-gh-pages/index.html"
   },
   {
-    "revision": "0a8a6ef6dd954dc6d974",
+    "revision": "115503c375dff0a8e184",
     "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-gh-pages/static/js/2.5dc4a546.chunk.js"
   },
   {
-    "revision": "0a8a6ef6dd954dc6d974",
-    "url": "/react-gh-pages/static/js/main.9b372be8.chunk.js"
+    "revision": "115503c375dff0a8e184",
+    "url": "/react-gh-pages/static/js/main.d7c93ea8.chunk.js"
   },
   {
     "revision": "5c14bf8d78ce841fcbb2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.8b6090fcd9fbf9476fc9427ece2695f9.js"
+  "/react-gh-pages/precache-manifest.4d442580c10dc8f8d45b628f6829d28f.js"
 );
 
 self.addEventListener('message', (event) => {
