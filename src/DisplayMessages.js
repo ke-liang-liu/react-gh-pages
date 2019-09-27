6c89609242c041cd465d723bf059e791fc3eb4d9
Keliang Liu <keliangliu1@gmail.com>
2019-09-27 18:13:05 +1000

    React DisplayMessages app with Redux

--- src/DisplayMessages.js
+++ src/DisplayMessages.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import {createStore} from 'redux';
+import {Provider,connect} from 'react-redux';
 
+// Redux:
+const ADD = 'ADD';
+// action creator
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message: message
+  }
+};
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(messageReducer);
+
+// React:  ******************************************
 class DisplayMessages extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: '',
-      messages: []
+      input: ''
+      // messages: []    move messages from local state to Redux store props
     }
     this.handleChange = this.handleChange.bind(this);
     this.submitMessage = this.submitMessage.bind(this);
@@ -19,21 +46,21 @@ class DisplayMessages extends React.Component {
   }
   submitMessage() {
     this.setState({
-      messages: this.state.messages.concat(this.state.input),
       input: ''
-    })
+      // messages: this.state.messages.concat(this.state.input) 
+    });
+    this.props.submitNewMessage(this.state.input);
   }
   render() {
-    let list = this.state.messages.map(x => <li key={x + 1}>{x}</li>);
+    let list = this.props.messages.map(x => <li key={x + 1}>{x}</li>);
     return (
       <div>
         <h2>Type in a new Message:</h2>
-        { /* render an input, button, and ul here */}
         <input type='text'
           value={this.state.input}
-          onChange={this.handleChange}  
-          onKeyUp={ event => {
-            if (event.key === 'Enter') {this.submitMessage()}
+          onChange={this.handleChange}
+          onKeyUp={event => {
+            if (event.key === 'Enter') { this.submitMessage() }
           }}
           autoFocus />
         <button onClick={this.submitMessage}>
@@ -42,10 +69,33 @@ class DisplayMessages extends React.Component {
         <ul>
           {list}
         </ul>
-        { /* change code above this line */}
       </div>
     );
   }
 };
 
-export default DisplayMessages;
+const mapStateToProps = (state) => {
+  return {messages: state}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message))
+    }
+  }
+};
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(DisplayMessages);
+
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Container/>
+      </Provider>
+    );
+  }
+};
+
+export default AppWrapper;
